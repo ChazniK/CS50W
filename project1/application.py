cdfe1dada1ba5acb99b73654e92730268f226f47
Chazni KATZ <ckatz@e4r19p5.wethinkcode.co.za>
2018-12-09 11:25:18 +0200

    Move files from original project to provided project1 folder

---
+++ project1/application.py
@@ -0,0 +1,42 @@
+# import os
+
+# from flask import Flask, session
+# from flask_session import Session
+# from sqlalchemy import create_engine
+# from sqlalchemy.orm import scoped_session, sessionmaker
+
+# app = Flask(__name__)
+
+# # Check for environment variable
+# if not os.getenv("DATABASE_URL"):
+#     raise RuntimeError("DATABASE_URL is not set")
+
+# # Configure session to use filesystem
+# app.config["SESSION_PERMANENT"] = False
+# app.config["SESSION_TYPE"] = "filesystem"
+# Session(app)
+
+# # Set up database
+# engine = create_engine(os.getenv("DATABASE_URL"))
+# db = scoped_session(sessionmaker(bind=engine))
+
+
+# @app.route("/")
+# def index():
+#     return "Project 1: TODO"
+
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def register():
+    return render_template("register.html")
+
+@app.route("/info", methods=["POST"])
+def info():
+    username = request.form.get("username")
+    email = request.form.get("email")
+    password = request.form.get("password")
+    confirm_password = request.form.get("confrim_password")
+    return render_template("info.html", username=username, email=email, password=password, confirm_password=confirm_password)
