57727372dbda0f035d2b64d4005a7f9435ee1e39
Chazni KATZ <ckatz@e4r19p5.wethinkcode.co.za>
2018-12-10 17:17:00 +0200

    Able to import csv data to database

--- project1/import.py
+++ project1/import.py
@@ -12,4 +12,19 @@ engine = create_engine(os.getenv("DATABASE_URL"))
 # database are kept separate
 db = scoped_session(sessionmaker(bind=engine))
 
-f = 
+def main():
+    # books = db.execute("SELECT * FROM books").fetchall()
+    # for book in books:
+    #     print(book) 
+    f = open("books.csv")
+    reader = csv.reader(f)
+    for isbn, title, author, year in reader:
+        db.execute("""
+        INSERT INTO books(isbn, title, author, year) 
+        VALUES(:isbn, :title, :author, :year)""",
+        {"isbn": isbn, "title": title, "author": author, "year": year})
+
+    db.commit()
+
+if __name__ == "__main__":
+    main()
