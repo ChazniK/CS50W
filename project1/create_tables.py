9e31e37465ffe33daf2ac9b9bd10e72fa5bf4712
Chazni KATZ <ckatz@e4r19p5.wethinkcode.co.za>
2018-12-10 09:37:50 +0200

    Working on functions to create tables

--- project1/create_tables.py
+++ project1/create_tables.py
@@ -5,9 +5,8 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 
 # database engine object from SQLAlchemy that manages connections to the database
 # DATABASE_URL is an environment variable that indicates where the database lives
-engine = create_engine(os.getenv("DATABASE_URL"))
+engine = create_engine("DATABSE_URL")
 
 # create a 'scoped session' that ensures different users' interactions with the
 # database are kept separate
 db = scoped_session(sessionmaker(bind=engine))
-
